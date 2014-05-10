c82598625ffd4fb654a4e6feeee7cb1f42aa04a1
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-05-09 20:56:29 -0700

    input.c output.s rc.s

--- output.s
+++ output.s
@@ -1,118 +1,33 @@
-/*
- * Generated Fri May 09 20:40:46 PDT 2014
- */
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!! CSE 131 Floating-Point Output Routine
+!! For displaying float types in RC
+!! Author: Garo Bournoutian, Winter 2008
+!!
+!!  NOTE: Pass float to be printed in %f0
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-	.section ".rodata"
-_endl:		.asciz "\n"
-_intFmt:	.asciz "%d"
-_strFmt:	.asciz "%s"
-_boolT:		.asciz "true"
-_boolF:		.asciz "false"
+	.section 	".rodata"
+	.align		4
+rFmt:	.asciz		"%.2lf"
 
-	.section ".bss"
-	.global	b
-	.align 4
-b:	.skip 4
+	.section	".text"
+	.align 		4
 
-	.section ".text"
-	.align 4
-	.global main
-main:
-	set	SAVE.main, %g1
-	save	%sp, %g1, %sp
-! local variable:   a    without init, just add offset
-	set	4, %l0
-	set	_intFmt, %o0
-	mov	%l0, %o1
-	call	printf
-	nop
-	set	_endl, %o0
-	call	printf
-	nop
+	.global		printFloat
 
-	set	4, %l0
-	set	_intFmt, %o0
-	mov	%l0, %o1
-	call	printf
-	nop
-	set	_endl, %o0
-	call	printf
-	nop
+printFloat:
+save	%sp, -(92 + 8) & -8, %sp	! save room for our double
 
-	set	4, %l0
-	set	_intFmt, %o0
-	mov	%l0, %o1
-	call	printf
-	nop
-	set	_endl, %o0
-	call	printf
-	nop
+fstod	%f0, %f0	! Promote float to double (even register)
+	std	%f0, [%fp-8]	! Store double into 8-aligned memory
 
-	set	4, %l0
-	set	_intFmt, %o0
-	mov	%l0, %o1
-	call	printf
-	nop
-	set	_endl, %o0
-	call	printf
-	nop
+	set	rFmt, %o0	! Set double format string
+	ld	[%fp-8], %o1	! Load double into int registers for printf
+	ld	[%fp-4], %o2
+	call	printf		! Call printf
+	nop			! Nop after function call
 
-	set	24, %l0
-	set	_intFmt, %o0
-	mov	%l0, %o1
-	call	printf
-	nop
-	set	_endl, %o0
-	call	printf
-	nop
+	ret			! Return, we're done!
+restore			! Restore register window (slide baby!)
 
-	set	4, %l0
-	set	_intFmt, %o0
-	mov	%l0, %o1
-	call	printf
-	nop
-	set	_endl, %o0
-	call	printf
-	nop
-
-	set	4, %l0
-	set	_intFmt, %o0
-	mov	%l0, %o1
-	call	printf
-	nop
-	set	_endl, %o0
-	call	printf
-	nop
-
-	set	4, %l0
-	set	_intFmt, %o0
-	mov	%l0, %o1
-	call	printf
-	nop
-	set	_endl, %o0
-	call	printf
-	nop
-
-	set	4, %l0
-	set	_intFmt, %o0
-	mov	%l0, %o1
-	call	printf
-	nop
-	set	_endl, %o0
-	call	printf
-	nop
-
-	set	24, %l0
-	set	_intFmt, %o0
-	mov	%l0, %o1
-	call	printf
-	nop
-	set	_endl, %o0
-	call	printf
-	nop
-
-	ret
-	restore
-
-! from DoFuncDecl2
-	SAVE.main = -(92 + 24) & -8
---
+++ rc.s
@@ -0,0 +1,118 @@
+/*
+ * Generated Fri May 09 20:53:08 PDT 2014
+ */
+
+	.section ".rodata"
+_endl:		.asciz "\n"
+_intFmt:	.asciz "%d"
+_strFmt:	.asciz "%s"
+_boolT:		.asciz "true"
+_boolF:		.asciz "false"
+
+	.section ".bss"
+	.global	b
+	.align 4
+b:	.skip 4
+
+	.section ".text"
+	.align 4
+	.global main
+main:
+	set	SAVE.main, %g1
+	save	%sp, %g1, %sp
+! local variable:   a    without init, just add offset
+	set	4, %l0
+	set	_intFmt, %o0
+	mov	%l0, %o1
+	call	printf
+	nop
+	set	_endl, %o0
+	call	printf
+	nop
+
+	set	4, %l0
+	set	_intFmt, %o0
+	mov	%l0, %o1
+	call	printf
+	nop
+	set	_endl, %o0
+	call	printf
+	nop
+
+	set	4, %l0
+	set	_intFmt, %o0
+	mov	%l0, %o1
+	call	printf
+	nop
+	set	_endl, %o0
+	call	printf
+	nop
+
+	set	4, %l0
+	set	_intFmt, %o0
+	mov	%l0, %o1
+	call	printf
+	nop
+	set	_endl, %o0
+	call	printf
+	nop
+
+	set	24, %l0
+	set	_intFmt, %o0
+	mov	%l0, %o1
+	call	printf
+	nop
+	set	_endl, %o0
+	call	printf
+	nop
+
+	set	4, %l0
+	set	_intFmt, %o0
+	mov	%l0, %o1
+	call	printf
+	nop
+	set	_endl, %o0
+	call	printf
+	nop
+
+	set	4, %l0
+	set	_intFmt, %o0
+	mov	%l0, %o1
+	call	printf
+	nop
+	set	_endl, %o0
+	call	printf
+	nop
+
+	set	4, %l0
+	set	_intFmt, %o0
+	mov	%l0, %o1
+	call	printf
+	nop
+	set	_endl, %o0
+	call	printf
+	nop
+
+	set	4, %l0
+	set	_intFmt, %o0
+	mov	%l0, %o1
+	call	printf
+	nop
+	set	_endl, %o0
+	call	printf
+	nop
+
+	set	24, %l0
+	set	_intFmt, %o0
+	mov	%l0, %o1
+	call	printf
+	nop
+	set	_endl, %o0
+	call	printf
+	nop
+
+	ret
+	restore
+
+! from DoFuncDecl2
+	SAVE.main = -(92 + 24) & -8
